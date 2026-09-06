f5d59cbdc66b2d1d54fd5c4d59a3f01a135e9690
whiskyy <wizfelix2cc@gmail.com>
2026-09-07 02:34:39 +0800

    sync 2026-09-07 02:34

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 离线缓存：首次打开后即可断网使用
-const CACHE = 'beijiang-v67';
+const CACHE = 'beijiang-v68';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
 });
